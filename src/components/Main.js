9c0c77a31b92ca34fb76fcc8b241af4be8cffb0b
Matias Cardone <zero.xm7@gmail.com>
2021-09-13 21:57:21 -0300

    updated home links

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,7 +11,7 @@ function Main(props) {
             <div className="container-fluid">
                 
             <button className="btn-cyan text-white btn ">Buy Now</button>
-                <Link className="navbar-brand" to="/">
+                <Link className="navbar-brand" to="/react-diary">
                     <img src={brand} alt="brand-logo" height="75"/>
                 </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -19,10 +19,10 @@ function Main(props) {
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div className="navbar-nav">
-                    <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-                    <Link className="nav-link"  to="/">Features</Link>
-                    <Link className="nav-link"  to="/">Pricing</Link>
-                    <Link className="nav-link disabled" to="/" tabIndex="-1" aria-disabled="true">Disabled</Link>
+                    <Link className="nav-link active" aria-current="page" to="/react-diary">Home</Link>
+                    <Link className="nav-link"  to="/react-diary">Features</Link>
+                    <Link className="nav-link"  to="/react-diary">Pricing</Link>
+                    <Link className="nav-link disabled" to="/react-diary" tabIndex="-1" aria-disabled="true">Disabled</Link>
                 </div>
                 </div>
             </div>
